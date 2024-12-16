83d5cfe7ef70f6d06e1be72ff11b6c5aef150aa8
Bubbles-cyper <jonathan.vera913@gmail.com>
2024-12-16 14:48:23 -0500

    EnglishDictionary.cpp

--- EnglishDictionary.cpp
+++ EnglishDictionary.cpp
@@ -10,6 +10,14 @@ void EnglishDictionary::add(std::string word, std::string definition) {
     dictionary.put(word, definition);
 }
 
+void EnglishDictionary::removeFirst(std::string word) {
+    dictionary.eraseFirst(word);
+}
+
+void EnglishDictionary::removeAll(std::string word) {
+    dictionary.erase(word);
+}
+
 void EnglishDictionary::printDictionary(bool fromStart) const {
     dictionary.print(fromStart);
 }
